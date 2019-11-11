2770dffe075a2623860795f67c91e8893a2c449d
RomanDevyatov <44396617+RomanDevyatov@users.noreply.github.com>
2019-11-11 22:52:33 +0300

    Update Book.java
    Hash equals

--- src/com/NetCracker/Book.java
+++ src/com/NetCracker/Book.java
@@ -57,4 +57,28 @@ public class Book extends Author{
         tmp=tmp.substring(0, tmp.length()-2);
         return tmp;
     }
+
+@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Book book = (Book) o;
+        return Double.compare(book.price, price) == 0 &&
+                qty == book.qty &&
+                Objects.equals(name, book.name) &&
+                Arrays.equals(authors, book.authors);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31*name.hashCode();
+        for(int i=0;i<authors.length;i++){
+            result = 31*result + authors[i].hashCode();
+        }
+        result = 31*result + (int)Double.doubleToLongBits(price);
+        result = 31*result + qty;
+        return result;
+    }
 }
